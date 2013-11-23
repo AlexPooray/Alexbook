26fa86d7dedafbbeb5f6aa42a5694545a92ee385
Alex Puray <merlinti325@Christophers-MacBook-Pro.local>
2013-11-22 20:35:06 -0500

    Created message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alexbook::Application.config.secret_token = 'eb47eaccef121810d1f1ffefebac8608abe8ab13c3649c1dc50a1f9292ade0c4f1381344e1b072b43201722784f83a265ed4f7655f52e959cd312374d8273288'
